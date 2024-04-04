c13994c398f67c18b6b2b01872cf28920eca6e7b
TheMysterys <the-mystery@outlook.com>
2024-04-05 11:21:52 +1300

    Added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
+	import { dev } from "$app/environment";
 	import "../app.css";
 </script>
 
 <slot />
+{#if !dev}
+	<!-- Cloudflare Web Analytics -->
+	<script
+		defer
+		src="https://static.cloudflareinsights.com/beacon.min.js"
+		data-cf-beacon={'{"token": "f6aa8136d824417b972f0c496b792711"}'}
+	></script>
+	<!-- End Cloudflare Web Analytics -->
+{/if}
