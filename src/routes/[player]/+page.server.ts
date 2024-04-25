955c85e406cb3cd3ea282af5e9715760ef776ae5
TheMysterys <the-mystery@outlook.com>
2024-04-25 14:37:16 +1200

    Removing the [/player] route. Causing issues with finding files

--- src/routes/[player]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = async ({ params }) => {
-	const searchedPlayer = params.player;
-
-	// Redirect to the player's page
-	redirect(302, `/player/${searchedPlayer}`);
-}
